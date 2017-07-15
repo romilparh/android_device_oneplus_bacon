3d090c4a8c1af4b05b737a607fdf465d017a5efa
Sultanxda <sultanxda@gmail.com>
2017-07-14 21:10:52 -0700

    Revert "bacon: Use MAP_NORESERVE for adreno's mmap calls"
    No-op since /proc/sys/vm/overcommit_memory is set to 1 (always overcommit).
    
    This reverts commit 0f1b3e51fc8475df01722d06d9121595ff27f3d7.

--- libshims/adreno_shim.cpp
+++ libshims/adreno_shim.cpp
@@ -14,34 +14,16 @@
  * limitations under the License.
  */
 
-#include <errno.h>
-#include <unistd.h>
 #include <sys/mman.h>
-
-#define BIONIC_ALIGN(value, alignment) \
-  (((value) + (alignment) - 1) & ~((alignment) - 1))
 
 #define MMAP2_SHIFT 12 // 2**12 == 4096
 
 extern "C" {
 
-void *__mmap2(void *, size_t, int, int, int, size_t);
+void *__mmap2(void*, size_t, int, int, int, size_t);
 
-void *mmap65(void *addr, size_t size, int prot, int flags, int fd, off64_t offset)
-{
-    if (offset < 0 || (offset & ((1UL << MMAP2_SHIFT) - 1))) {
-        errno = EINVAL;
-        return MAP_FAILED;
-    }
-
-    // prevent allocations large enough for `end - start` to overflow
-    size_t rounded = BIONIC_ALIGN(size, PAGE_SIZE);
-    if (rounded < size || rounded > PTRDIFF_MAX) {
-        errno = ENOMEM;
-        return MAP_FAILED;
-    }
-
-    return __mmap2(addr, size, prot, flags | MAP_NORESERVE, fd, offset >> MMAP2_SHIFT);
+void *mmap65(void* addr, size_t size, int prot, int flags, int fd, off64_t offset) {
+    return __mmap2(addr, size, prot, flags, fd, offset >> MMAP2_SHIFT);
 }
 
 }
